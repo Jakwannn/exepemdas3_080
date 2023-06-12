0833572a16e02fa7e5a583b19f433840cfc6164f
Naufal Dzakwan <naufal.dzakwan.ft19@mail.umy.ac.id>
2023-06-12 10:25:22 +0700

    Add project files.

---
+++ UCP2/UCP2.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+class bidangDatar {
+private:
+    int x;
+public:
+    bidangDatar() {
+        x = 0;
+    }
+
+    virtual void input(int i) {
+        x[i] = value;
+    }
+
+    virtual void Luas(int a) {
+
+
+    return 0;
+}
+
+    virtual void Keliling(int a) {
+
+
+
+    return 0;
+}
+
+    void setX(int a) {
+
+
+    this->x = a;
+}
+
+    int getX() {
+
+
+    return x;
+}
+
+};
+
+class Lingkaran :public bidangDatar {
+
+};
+
+class BujurSangkar :public bidangDatar {
+
+};
+
+int main()
+{
+    
+}
+
+
